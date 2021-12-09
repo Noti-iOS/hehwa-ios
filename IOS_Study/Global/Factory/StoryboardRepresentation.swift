714f06d0ea201434d5104fade4b0119df2afbdc1
yunkyung-Hwang <dudhkd98@naver.com>
2021-12-09 18:16:39 +0900

    commit

---
+++ IOS_Study/Global/Factory/StoryboardRepresentation.swift
@@ -0,0 +1,13 @@
+//
+//  StoryboardRepresentation.swift
+//  IOS_Study
+//
+//  Created by 황윤경 on 2021/12/09.
+//
+
+import Foundation
+struct StoryboardRepresentation {
+    let bundle: Bundle?
+    let storyboardName: String
+    let storyboardId: String
+}
---
+++ IOS_Study/Global/Factory/TypeOfViewController.swift
@@ -0,0 +1,9 @@
+//
+//  TypeOfViewController.swift
+//  IOS_Study
+//
+//  Created by 황윤경 on 2021/12/09.
+//
+
+import Foundation
+
