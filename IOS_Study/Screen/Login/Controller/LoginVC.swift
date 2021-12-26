84c349d6db1184beaf4b6a539d5e3a24ac4e8404
hojoon <ujuj0202@gmail.com>
2021-12-27 00:49:09 +0900

    [FEAT]: 로그인 기본 화면 구성 구현

--- IOS_Study/Screen/Login/Controller/LoginVC.swift
+++ IOS_Study/Screen/Login/Controller/LoginVC.swift
@@ -2,15 +2,60 @@
 //  LoginVC.swift
 //  IOS_Study
 //
-//  Created by 황윤경 on 2021/12/18.
 //
 
 import UIKit
 
 class LoginVC: UIViewController {
-
+    @IBOutlet weak var logo: UIImageView!
+    @IBOutlet weak var loginLabel: UILabel!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var googleLogin: UIButton!
+    @IBOutlet weak var kakaoLogin: UIButton!
+    @IBOutlet weak var inputEmail: UITextField!
+    @IBOutlet weak var inputPassword: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setView()
+        setSocialLoginButton()
+    }
+}
 
+// MARK: - custom Method
+extension LoginVC{
+    func setView () {
+        let logoImage = UIImage(named: "Logo")
+        logo.image = logoImage
+        loginLabel.alpha = 0.5
+        loginButton.layer.cornerRadius = 4
+        inputEmail.addLeftPadding()
+        inputEmail.layer.cornerRadius = 4
+        inputPassword.addLeftPadding()
+        inputPassword.layer.cornerRadius = 4
+    }
+    
+    // 소셜 로그인 버튼 그림자 추가
+    func setSocialLoginButton () {
+        googleLogin.layer.cornerRadius = 4
+        googleLogin.layer.shadowColor = CGColor(red: 0, green: 0, blue: 0, alpha: 0.25)
+        googleLogin.layer.shadowRadius = 3
+        googleLogin.layer.shadowOpacity = 0.7
+        googleLogin.layer.shadowOffset = CGSize(width: 0, height: 1)
+        kakaoLogin.layer.cornerRadius = 4
+        kakaoLogin.layer.shadowColor = CGColor(red: 0, green: 0, blue: 0, alpha: 0.25)
+        kakaoLogin.layer.shadowRadius = 3
+        kakaoLogin.layer.shadowOpacity = 0.7
+        kakaoLogin.layer.shadowOffset = CGSize(width: 0, height: 1)
+    }
+}
+
+// MARK: - Custom Method
+extension UITextField {
+    func addLeftPadding() {
+        // text 입력 부분 왼쪽 패딩을 위한 함수
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: self.frame.height))
+        self.leftView = paddingView
+        self.leftViewMode = ViewMode.always
     }
 }
