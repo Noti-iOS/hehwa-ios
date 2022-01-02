b38640d341b4cc3ff9af64c8f0cf7df14de302bc
Hwang <hyk0425@swu.ac.kr>
2022-01-03 01:19:49 +0900

    sideMenu - 리스트별 Action

--- IOS_Study/Screen/Home/Controller/SideMenuVC.swift
+++ IOS_Study/Screen/Home/Controller/SideMenuVC.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class SideMenuVC: UIViewController {
-    @IBOutlet weak var MenuListTV: UITableView!
+    @IBOutlet weak var menuListTV: UITableView!
     
     let menuList = ["버전 정보", "로그아웃", "과외 정보", "회원 정보 수정"]
     let menuIcon = [UIImage(named: "Menu_Version"), UIImage(named: "Menu_Logout"), UIImage(named: "Menu_info"), UIImage(named: "Menu_Setting")]
@@ -25,24 +25,57 @@ extension SideMenuVC {
     }
     
     func setUpEtcTV() {
-        MenuListTV.dataSource = self
+        menuListTV.dataSource = self
+        menuListTV.delegate = self
         
-        MenuListTV.backgroundColor = .systemGray6
-        MenuListTV.isScrollEnabled = false
+        menuListTV.backgroundColor = .systemGray6
+        menuListTV.isScrollEnabled = false
     }
 }
 
-// MARK:UITableViewDataSource
+// MARK: UITableViewDataSource
 extension SideMenuVC: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 4
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = MenuListTV.dequeueReusableCell(withIdentifier: Identifiers.menuListTVC, for: indexPath) as! MenuListTVC
+        let cell = menuListTV.dequeueReusableCell(withIdentifier: Identifiers.menuListTVC, for: indexPath) as! MenuListTVC
         cell.backgroundColor = .systemGray6
         cell.menuTitle.text = menuList[indexPath.row]
         cell.menuIcon.image = menuIcon[indexPath.row]
         return cell
+    }
+}
+
+// MARK: UITableViewDelegate
+extension SideMenuVC: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // 선택 후 해제
+        menuListTV.deselectRow(at: indexPath, animated: false)
+        
+        // cell에 맞는 Action
+        switch indexPath.row {
+        case 0:
+            print("버전 정보")
+        case 1:
+            let logoutAC = UIAlertController(title: "", message: "로그아웃 하시겠습니까?", preferredStyle: .alert)
+            logoutAC.view.tintColor = .white
+            let logout = UIAlertAction(title: "로그아웃", style: .destructive) { (action) in
+                print("로그아웃")
+            }
+            let cancel = UIAlertAction(title: "취소", style: .cancel) { action -> Void in }
+
+            logoutAC.addAction(logout)
+            logoutAC.addAction(cancel)
+
+            present(logoutAC, animated: true, completion: nil)
+        case 2:
+            print("과외 정보")
+        case 3:
+            print("회원 정보 수정")
+        default:
+            return
+        }
     }
 }
